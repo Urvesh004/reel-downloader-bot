c1bbb9010e98d7ffb555c7d23d8dc004fd05116b
Urvesh004 <urveshmovaliya@gmail.com>
2026-02-19 11:52:52 +0530

    UPDATE: Message

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ async def download_instagram(update: Update, context: ContextTypes.DEFAULT_TYPE)
         # await update.message.reply_text("❌ Send valid Instagram link")
         return
 
-    await update.message.reply_text("Downloading... ⏳")
+    # await update.message.reply_text("Downloading... ⏳")
 
     try:
         # remove query parameters (?...)
